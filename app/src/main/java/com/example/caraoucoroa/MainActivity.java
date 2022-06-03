d7a6700c90f633a475412b18ce3dbd5a4d13ddec
FelipeEnne <felipeenne@gmail.com>
2022-06-02 21:05:54 -0300

    finish the app

--- app/src/main/java/com/example/caraoucoroa/MainActivity.java
+++ app/src/main/java/com/example/caraoucoroa/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView playImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playImageView = findViewById(R.id.playImageView);
+
+        playImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+
+                int number = new Random().nextInt(2);
+                intent.putExtra("number", number);
+
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/caraoucoroa/ResultActivity.java
+++ app/src/main/java/com/example/caraoucoroa/ResultActivity.java
@@ -2,13 +2,41 @@ package com.example.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private ImageView imageResult;
+    private ImageView imageViewBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        imageResult = findViewById(R.id.imageResult);
+        imageViewBack = findViewById(R.id.imageViewBack);
+
+        Bundle dados = getIntent().getExtras();
+        int numero = dados.getInt("number");
+
+        if(numero == 0) {
+            imageResult.setImageResource(R.drawable.moeda_cara);
+        } else {
+            imageResult.setImageResource(R.drawable.moeda_coroa);
+        }
+
+        imageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
